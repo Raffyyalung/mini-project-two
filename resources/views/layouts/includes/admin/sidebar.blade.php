5ca579dc738cad03725b8acea5e2117f481496d4
rafyalung <raffy26026@gmail.com>
2023-03-30 05:43:49 +0800

    start

--- resources/views/layouts/includes/admin/sidebar.blade.php
+++ resources/views/layouts/includes/admin/sidebar.blade.php
@@ -9,17 +9,7 @@
                 </a>
                 <div class="sb-sidenav-menu-heading">Interface</div>
                 {{-- <hr class="dropdown-divider" /> --}}
-                <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
-                    <div class="sb-nav-link-icon"><i class="fa-solid fa-tags"></i></i></div>
-                    Categories
-                    <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                </a>
-                <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
-                    <nav class="sb-sidenav-menu-nested nav">
-                        <a class="nav-link" href="{{route('admin.category')}}">Category List</a>
-                        <a class="nav-link" href="{{route('admin.category.create')}}">Add Categories</a>
-                    </nav>
-                </div>
+                
                 <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePages" aria-expanded="false" aria-controls="collapsePages">
                     <div class="sb-nav-link-icon"><i class="fa-solid fa-box-open"></i></div>
                     Products
@@ -29,6 +19,18 @@
                     <nav class="sb-sidenav-menu-nested nav">
                         <a class="nav-link" href="{{route('admin.product')}}">Product List</a>
                         <a class="nav-link" href="{{route('admin.product.create')}}">Add Products</a>
+                    </nav>
+                </div>
+
+                <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
+                    <div class="sb-nav-link-icon"><i class="fa-solid fa-tags"></i></i></div>
+                    Categories
+                    <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                </a>
+                <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                    <nav class="sb-sidenav-menu-nested nav">
+                        <a class="nav-link" href="{{route('admin.category')}}">Category List</a>
+                        <a class="nav-link" href="{{route('admin.category.create')}}">Add Categories</a>
                     </nav>
                 </div>
             </div>
